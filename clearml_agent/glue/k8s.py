e30928d0bec064767ac349c1d32e51f41b51429d
Sven Ćurković <sven.curkovic@gmail.com>
2025-11-26 16:04:28 +0100

    fix: update Dockerfile.test

--- clearml_agent/glue/k8s.py
+++ clearml_agent/glue/k8s.py
@@ -94,7 +94,6 @@ class K8sIntegration(Worker):
         "[ ! -z $CLEARML_AGENT_NO_UPDATE ] || $LOCAL_PYTHON -m pip --version > /dev/null || export LOCAL_PYTHON=$(PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin command -v python3)",
         "rm -f /usr/lib/python3.*/EXTERNALLY-MANAGED",  # remove PEP 668
         "{extra_bash_init_cmd}",
-        "[ ! -z $CLEARML_AGENT_NO_UPDATE ] || $LOCAL_PYTHON -m pip install clearml-agent{agent_install_args}",
         "{extra_docker_bash_script}",
         "$LOCAL_PYTHON -m clearml_agent execute {default_execution_agent_args} --id {task_id}"
     ]
